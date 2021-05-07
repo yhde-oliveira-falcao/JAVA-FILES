fb784b67c56f33ea5253b6668e89bcab90fab0a5
YHOFalcao <55162954+yhde-oliveira-falcao@users.noreply.github.com>
2021-05-07 15:36:00 -0400

    Update and rename Main.java to NumberTriangle.java

--- NumberTriangle.java
+++ NumberTriangle.java
@@ -1,6 +1,6 @@
-package com.company;
+//package com.company;
 
-public class Main {
+public class NumberTriangle {
 
     public static void main(String[] args) {
         System.out.println("Starting the pyramid of numbers");
